dbe7e76543487619078acd632ff2bbfd19978ef5
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-05-29 10:39:56 +0000

    Add content

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
-import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
+import css from "./App.module.css";
 // import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className={css.App}>
-      <Sidebar />
+      {/* Add your components here */}
       <NavBarForm />
+      <Sidebar />
+      <Content />
     </div>
   );
 }
