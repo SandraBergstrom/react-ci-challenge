106f8d6d0682abf7384a278c3b42a3b29fd3d892
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-05-31 12:19:09 +0000

    API

---
+++ my-app/src/components/ContentAPIHooks.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import PostItem from "./PostItem";
+import Loader from "./Loader";
+import axios from 'axios';
+import API_KEY from './secrets';
+
+function ContentAPIHooks() {
+  return (
+    <div>ContentAPIHooks</div>
+  )
+}
+
+export default ContentAPIHooks
